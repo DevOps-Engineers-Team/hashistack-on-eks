d6154decd1897bc1856e7dae1011de259eff4552
WitoldSlawko <witold.slawko@gmail.com>
2023-04-25 15:29:28 +0200

    removed legacy module

--- core-infra/develop/aws-alb-ctrl/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "core-infra-gitops-terraform-backend"
-    key = "develop/eks-demo-alb-ctrl/terraform.state"
-    region = "eu-west-1"
-    dynamodb_table = "terraform_lock"
-  }
-}
--- core-infra/develop/aws-alb-ctrl/main.tf
+++
@@ -1,9 +0,0 @@
-module "ingress_alb" {
-  source = "../../../modules/aws-alb-ctrl"
-  environment   = local.environment
-  config_name = local.config_name
-  account_id    = local.account_id
-
-  cluster_name = local.cluster_name
-  k8s_namespace = local.k8s_namespace
-}
