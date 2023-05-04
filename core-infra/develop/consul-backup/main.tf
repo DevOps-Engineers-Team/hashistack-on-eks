6c012f624f4fac54deab845212640f3afa9cea35
WitoldSlawko <witold.slawko@gmail.com>
2023-05-04 20:12:40 +0200

    snapshot backups are done locally first

---
+++ core-infra/develop/consul-backup/main.tf
@@ -0,0 +1,23 @@
+resource "null_resource" "consul_backup" {
+
+   triggers = {
+    timestamp = "${replace("${timestamp()}", "/[-| |T|Z|:]/", "")}"
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+        aws sts get-caller-identity
+        aws eks update-kubeconfig --region eu-west-1 --name $CLUSTER_NAME
+        SNAPSHOT=$(date +%y-%m-%d-%H-%m-%S | sed 's/\"//g')
+        CONSUL_LEADER_NODE=$(kubectl -n consul exec consul-develop-consul-server-1 -- sh -c "export CONSUL_HTTP_TOKEN=$CONSUL_TOKEN && consul operator raft list-peers | grep leader | sed 's/ /\n/g' | grep consul | sed 's/\"//g'")
+        kubectl -n consul exec $CONSUL_LEADER_NODE -- sh -c "export CONSUL_HTTP_TOKEN=$CONSUL_TOKEN && cd /consul/extra-config && consul snapshot save backup-$SNAPSHOT.snap"
+        kubectl cp consul/$CONSUL_LEADER_NODE:/consul/extra-config/backup-$SNAPSHOT.snap ./backup-$SNAPSHOT.snap
+        
+    EOT
+
+    environment = {
+      CLUSTER_NAME = local.cluster_name
+      CONSUL_TOKEN = data.kubernetes_secret.tool_token.data["token"]
+    }
+  }
+}
---
+++ core-infra/develop/consul-backup/variables.tf
@@ -0,0 +1,24 @@
+locals {
+  environment   = basename(dirname(path.cwd))
+  config_name = basename(dirname(dirname(path.cwd)))
+  tool_name = "consul"
+  application  = "gitops"
+  cluster_name = "${local.config_name}-${local.environment}-${local.application}-cluster"
+}
+
+variable "asg_name" {
+    default = "consul-asg"
+}
+
+variable "consul_version" {
+    default = "1.15.2"
+}
+
+variable "datacenter_name" {
+  default = "aws-eks"
+}
+
+variable "ssm_doc_commands" {
+  type = list(string)
+  default = ["systemctl stop consul"]
+}
