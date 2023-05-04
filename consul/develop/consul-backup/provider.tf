0c13f9b178e7281333c2274fc46492b17f106375
WitoldSlawko <witold.slawko@gmail.com>
2023-05-04 20:15:11 +0200

    reoorganized consul into separate main folder

--- consul/develop/consul-backup/provider.tf
+++ consul/develop/consul-backup/provider.tf
@@ -7,9 +7,3 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
 }
-
-provider "consul" {
-  address    = "${local.tool_address}"
-  scheme = "https"
-  token = data.kubernetes_secret.tool_token.data["token"]
-}
